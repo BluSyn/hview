df8dc317a51e814f58f90c25a37d5293b66a9f4d
Steven Bower <bowerblu@gmail.com>
2020-10-06 03:14:04 +0000

    Working entry lists

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@
 use std::io;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::collections::HashMap;
 
+use rand::seq::IteratorRandom;
 use serde::{Serialize, Deserialize};
 use rocket::http::Status;
 use rocket::request::Request;
@@ -14,6 +14,7 @@ use rocket::response::{self, Responder, NamedFile};
 use rocket::response::status::NotFound;
 use rocket_contrib::templates::Template;
 
+const THUMB_FORMAT: &str = "avif";
 const BASE_DIR: &str = "./test-fixture/";
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -21,8 +22,8 @@ struct TemplateEntry {
     name: String,
     href: String,
     size: u64,
-    thumb: String,
-    ext: String
+    thumb: Option<PathBuf>,
+    ext: Option<String>
 }
 
 impl TemplateEntry {
@@ -31,8 +32,8 @@ impl TemplateEntry {
             name: String::from(""),
             href: String::from(""),
             size: 0,
-            thumb: String::from(""),
-            ext: String::from("")
+            thumb: None,
+            ext: None
         }
     }
 }
@@ -42,7 +43,7 @@ struct TemplatePage {
     title: String,
     base_path: String,
     read_only: bool,
-    files: Vec<TemplateEntry>,
+    files:  Vec<TemplateEntry>,
     folders: Vec<TemplateEntry>
 }
 
@@ -84,29 +85,73 @@ impl<'a> Responder<'a> for CustomResponder {
     }
 }
 
-fn get_dir(path: &PathBuf) -> io::Result<Template> {
-    let mut page = TemplatePage::new();
+// Convert /dir/file.jpg -> /dir/.th/file.jpg.avif
+fn file_path_to_thumb(file: &PathBuf) -> Result<PathBuf, &'static str> {
+	let thumb_name = format!(".th/{}.{}",
+		file
+			.file_name().ok_or("Filename")?
+			.to_str().ok_or("Filename String")?,
+		THUMB_FORMAT);
+	let file_thumb = file
+		.parent().ok_or("Parent directory")?
+		.join(&thumb_name);
 
-    for entry in fs::read_dir(&path)? {
+	Ok(file_thumb)
+}
+
+// Get file inside dir that ends with THUMB_FORMAT, if exists
+fn get_random_thumb(path: &PathBuf) -> Option<PathBuf> {
+    if !path.is_dir() || !path.exists() {
+        return None;
+    }
+
+    let thumbs = fs::read_dir(&path).unwrap()
+        .filter_map(|d| {
+            let path = d.unwrap().path();
+            if path.extension().unwrap() == "avif" {
+                return Some(path);
+            }
+            None
+        });
+
+    let mut rng = rand::thread_rng();
+    Some(thumbs.choose(&mut rng).unwrap())
+}
+
+fn get_dir(dir: &PathBuf) -> io::Result<Template> {
+    let mut page = TemplatePage::new();
+    let thpath = dir.join(".th");
+
+    for entry in fs::read_dir(&dir)? {
         let entry = entry?;
         let path = entry.path();
+
+        // Skip existing th paths
+        if path == thpath {
+            continue;
+        }
+
         let meta = entry.metadata().unwrap();
+        let ext = path.extension();
 
         let mut details = TemplateEntry::new();
-        details.name = entry.file_name().to_os_string().into_string().unwrap();
-        details.href = String::from(path.to_path_buf().to_str().unwrap());
+        details.name = entry.file_name().to_str().unwrap().to_string();
+        details.href = path.to_path_buf().to_str().unwrap().to_string();
         details.size = meta.len();
-        details.ext = path.extension().unwrap().to_os_string().into_string().unwrap();
+        details.ext = if ext.is_none() {
+            None
+        } else {
+            Some(ext.unwrap().to_str().unwrap().to_string())
+        };
 
         if path.is_dir() {
+            details.thumb = get_random_thumb(&thpath);
             page.folders.push(details);
         } else {
+            details.thumb = file_path_to_thumb(&path).ok();
             page.files.push(details);
         }
     }
-
-    // let mut data = HashMap::new();
-    // data.insert("title", "Testing");
 
     Ok(Template::render("dir", &page))
 }
